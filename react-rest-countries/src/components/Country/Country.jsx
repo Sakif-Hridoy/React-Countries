2b50cb9ac27bc72c75d1a2f8755bdefcc17d341b
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-02 13:00:08 +0600

    again create country and add properties

--- react-rest-countries/src/components/Country/Country.jsx
+++ react-rest-countries/src/components/Country/Country.jsx
@@ -1,11 +1,15 @@
 import './Country.css'
 const Country = ({country}) => {
     // console.log(country)
-    const{name,flags} = country
+    const{name,flags,population,area,cca3} = country
     return (
         <div className='country'>
             <h3>{name.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population:{population}</p>
+            <p>area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button>Visited</button>
         </div>
     );
 };
