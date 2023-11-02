34dd54920850a2c21dac3117d94247eff9623620
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-02 12:50:22 +0600

    fix key and props eslint issues

--- react-rest-countries/src/components/Country/Country.jsx
+++ react-rest-countries/src/components/Country/Country.jsx
@@ -1,11 +1,11 @@
 import './Country.css'
 const Country = ({country}) => {
-// console.log(country)
-const {name,flags} = country;
+    // console.log(country)
+    const{name,flags} = country
     return (
         <div className='country'>
-            <h3>Name: {name?.common}</h3>
-            <img src={flags.png}></img>
+            <h3>{name.common}</h3>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
