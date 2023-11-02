162c5acc6c748bec45898b1ccd869b1c7d394492
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-02 23:29:55 +0600

    pass an eventhandler from one component to another

--- react-rest-countries/src/components/Country/Country.jsx
+++ react-rest-countries/src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountry}) => {
     // console.log(country)
     const{name,flags,population,area,cca3} = country;
 
@@ -10,13 +10,17 @@ const Country = ({country}) => {
         setVisited(!visited)
     }
 
+    console.log(handleVisitedCountry)
+
     return (
-        <div className='country'>
-            <h3>{name.common}</h3>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{color:visited ? 'purple':'black'}}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population:{population}</p>
             <p>area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisitedCountry} className='btn'>Mark Visited</button>
+            <br />
             <button   onClick={handleVisited} className='btn'>{visited ? 'Visited' : 'Going'}</button>
             <div>
             {visited ? 'I have visited this country' :'I want to visit'}
